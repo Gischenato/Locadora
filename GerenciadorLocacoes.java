34963f3ca50ef5a9c5f04b77c7cca67fe042879a
Giovanni Schenato <gischenato@gmail.com>
2021-11-19 18:32:37 -0300

    GerenciadorLocacoes

---
+++ GerenciadorLocacoes.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class GerenciadorLocacoes {
+     private ArrayList<Locacao> listaDeLocacoes;
+
+     public GerenciadorLocacoes(){
+          this.listaDeLocacoes = new ArrayList<>();
+     }
+
+     public void solicidaLocacao(int codCliente, int codFilme){
+          
+     }
+
+     public void solicitaDevolucao(){
+
+     }
+}
