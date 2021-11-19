64da78ca0f54c401d5bbff4517fbab306dca8c8f
Giovanni Schenato <gischenato@gmail.com>
2021-11-19 19:21:59 -0300

    solicitarLocacao()

--- GerenciadorLocacoes.java
+++ GerenciadorLocacoes.java
@@ -1,8 +1,9 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class GerenciadorLocacoes {
      private static GerenciadorLocacoes INSTANCE;
-     private ArrayList<Locacao> listaDeLocacoes;
+     public ArrayList<Locacao> listaDeLocacoes;
 
      public static GerenciadorLocacoes getInstance() {
           if (INSTANCE == null)
@@ -12,14 +13,40 @@ public class GerenciadorLocacoes {
      }
 
      private GerenciadorLocacoes() {
-          this.listaDeLocacoes = new ArrayList<>();
+          listaDeLocacoes = new ArrayList<>();
      }
 
-     public void solicidaLocacao(int codCliente, int codFilme) {
+     private void addLocacao(Locacao locacao) {
+          listaDeLocacoes.add(locacao);
+     }
+
+     public void solicitarLocacao(int codCliente, int codFilme) {
+          GerenciadorClientes clientes = GerenciadorClientes.getInstance();
+          GerenciadorFilmes filmes = GerenciadorFilmes.getInstance();
+
+          Cliente cliente = clientes.buscarCliente(codCliente);
+          Filme filme = filmes.buscarFilme(codFilme);
+
+          if (cliente == null && filme == null)
+               throw new IllegalArgumentException("Codigo de cliente e filme Invalido");
+          if (filme == null)
+               throw new IllegalArgumentException("Codigo de filme Invalido");
+          if (cliente == null)
+               throw new IllegalArgumentException("Codigo de cliente Invalido");
+
+          addLocacao(new Locacao(LocalDate.now(), cliente, filme));
+     }
+
+     public void solicitarDevolucao() {
 
      }
 
-     public void solicitaDevolucao() {
-
+     @Override
+     public String toString() {
+          String res = "Locacoes cadastradas:\n";
+          for (Locacao locacao : listaDeLocacoes) {
+               res += locacao.toString() + "\n";
+          }
+          return res;
      }
 }
